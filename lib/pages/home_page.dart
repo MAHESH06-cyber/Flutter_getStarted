5d2e6b4f96995234005a71281316da3a475a096d
MAHESH06-cyber <mchaudhari1201@gmail.com>
2021-07-03 12:23:35 +0530

    created a card ind duplicate the date forr scrolling purpose

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+import 'package:flutter_application_1/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   // const HomePage({Key? key}) : super(key: key);
@@ -7,15 +9,20 @@ class HomePage extends StatelessWidget {
   final String name = "Flutter";
   @override
   Widget build(BuildContext context) {
-    // double pi = 3.14;
-    // bool ismale = true;
+    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Testing ..."),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $day days of $name !"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
